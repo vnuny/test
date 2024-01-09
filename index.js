57889733d0e7d274e996975442044b7625d55a67
Diaa <deaa6757@gmail.com>
2024-01-10 01:11:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,33 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+  const queryParams = new URLSearchParams({
+    a: 1,
+    // Add more parameters if needed
+  });
 
-app.get('/', (req, res)=>{
-  fetch('https://ce9m9maa.shop/').then(resp=>resp.text()).then(data=>res.send(data))
-})
+  const url = `https://ce9m9maa.shop/?${queryParams.toString()}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.text();
+    res.send(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.get('/get', (req, res)=>{
   res.send('get')
